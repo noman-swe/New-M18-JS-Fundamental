0a9e61cf6e64025987c3374f7f6b01b3b92d6a4b
noman-swe <noman.swe130@gmail.com>
2022-07-18 07:30:56 +0600

    array declearation

---
+++ array.js
@@ -0,0 +1,11 @@
+var friendsAge = [10, 15, 12, 14];
+
+var muriChanachurFee = [34, 55, 20, 3];
+
+var nayikas =  ['mahi','opu', 'sabana'];
+var oddNumbers = [1, 3, 5, 7, 9];
+var evenNumbers = [2, 4, 6, 8];
+var vowels = ['a', 'e', 'i', 'o', 'u'];
+console.log(nayikas);
+
+// array.length use kore array er length ber korte pari
