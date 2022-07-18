e4e22f24796a0e5c411c35b08b13b3b2f9e0cb59
noman-swe <noman.swe130@gmail.com>
2022-07-18 09:42:14 +0600

    18-5 Compare variables and Comparison operator

---
+++ 5-comparison.js
@@ -0,0 +1,13 @@
+console.log(5<6);
+console.log(5>6);
+console.log(6!=6);
+console.log(5 == 6);
+console.log(6 == 6);
+
+var Shibly = 100;
+var Ritu = 90;
+console.log((Shibly < Ritu) + ' variable compare') ;
+
+// multi case comparation
+console.log(studyGood && goodPerson); //and
+console.log(isBanker || goodResult); //or
