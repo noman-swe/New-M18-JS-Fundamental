e60ba967a8d1c7e1a4f7639dbb9c876a7b2dbaa7
noman-swe <noman.swe130@gmail.com>
2022-07-18 09:55:55 +0600

    18-6 Make conditional decision, if-else, comparison

---
+++ 6-conditional.js
@@ -0,0 +1,14 @@
+var isFoodReady = false;
+if(isFoodReady == true){
+    console.log('khamu');
+}else{
+    console.log('let the cook finish');
+}
+
+var iPhone = 70000;
+var myBudget = 8900;
+if(iPhone < myBudget){
+    console.log('iphone dia futani korbo');
+}else{
+    console.log('My xaomi is the best');
+}
