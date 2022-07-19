a584991925f820ff135e304c4d29adef52146986
noman-swe <noman.swe130@gmail.com>
2022-07-19 09:00:13 +0600

    array loops

---
+++ 18-Extra/summary.js
@@ -0,0 +1,21 @@
+var num = [45, 86, 64, 75, 85, 58, 65, 13];
+/*
+for( var i = 0; i <7; i++){
+    var element = num[i];
+    console.log(element);
+}
+*/
+num.push(40);
+
+for (var i = 0; i < num.length; i++){
+    var element = num[i];
+    console.log(element);
+}
+
+
+// new array
+var items = ['bottle', 'mobile', 'charger', 'watch', 'book', 'paper'];
+for (var i = 0; i < items.length; i++){
+    var item = items[i];
+    console.log(item);
+}
